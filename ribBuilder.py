fc0385e5cee88c0604dcddfb4ce21f6286014e19
Tricide <cameronjspears@icloud.com>
2025-02-03 23:59:57 -0500

    real progress has been made.

--- ribBuilder.py
+++ ribBuilder.py
@@ -1,20 +1,37 @@
-import pandas as pd
+import dataSheet
 class rigBuilder:
     def __init__(self, restriction):
         ###Computer is a collection of components
         self.computer = []
         ###restriction is  a set of preset restrictions
         self.restriction = {
-            money: None, 
-            type: None
+            self.money: None, 
+            self.ctype: None
         }
+        
+        dataLists = dataSheet.Datasheet("databaseList.csv")[0]
+        for keys in dataLists:
+            dataLists[keys] = dataSheet.Datasheet[keys]
+            
+            
 
     ###to be employed during the construction of a rigBuilder settings
 
     ###begin to narrow down the selected; interact with data base, create a culled version
-    def applyRestrictions():
-        ###will interact with data sheet
-        smthDataSheet
+    def applyRestrictions(responseList):
+        ###will determine monetary restrictions prior to ranking
+        monetaryRestrictionsList = {
+            "gaming":[20,40,15,8,9,8],
+            "work/general": [30,30,15,8,9,8],
+            "powerhouse": [20,45,10,7,10,8]
+        }
+        
+        for restriction in responseList:
+            ### if (contains some restrictions):
+                ### delete some restriction from the dictionary
+        
+            
+        
     
     ###now dealing with culled data sheet, generate a dictionary of arrays, in which they are in order of priority
     def createPriorityListe():
