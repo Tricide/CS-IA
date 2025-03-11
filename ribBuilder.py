d98f8097b3ffb897c7a0e33382b73d3e09b9348a
Tricide <cameronjspears@icloud.com>
2025-03-10 23:57:39 -0400

    Just a couple lines of code away.

--- ribBuilder.py
+++ ribBuilder.py
@@ -4,6 +4,12 @@ class rigBuilder:
         ###Computer is a collection of components
         self.computer = []
         ###restriction is  a set of preset restrictions
+        
+        self.monetaryRestrictionsList = {
+            "gaming":[.20,.40,.15,.8,.9,.8],
+            "work/general/business": [.30,.30,.15,.8,.9,.8],
+            "powerhouse": [.20,.45,.10,.7,.10,.8]
+        }
         ##budget
         self.budget = budget
         
@@ -18,9 +24,11 @@ class rigBuilder:
         ##takes inputs from homePage in this order [cpu, gpu, mobo, memory, storage, psu]        
         self.inputList = input
         
-        dataLists = dataSheet.Datasheet("databaseList.csv")[0]
-        for key in dataLists:
-            dataLists[key] = dataSheet.Datasheet(dataLists[key])
+        self.toFind = self.applyRestrictions()
+        
+        self.dataLists = dataSheet.Datasheet("databaseList.csv")[0]
+        for key in self.dataLists:
+            self.dataLists[key] = dataSheet.Datasheet(self.dataLists[key])
             
             
             
@@ -32,7 +40,10 @@ class rigBuilder:
             
             
     def powerAlgorithm(self):
-        sigma='sigma'
+        ##search through every remaining dataList
+        for item in self.toFind:
+            dataList = 
+        
     
     def effectiveAlgorithm(self):
         sigma='sigma'
@@ -40,12 +51,37 @@ class rigBuilder:
     
 
     ###begin to narrow down the selected; interact with data base, create a culled version
-    def applyRestrictions(self, responseList):
+    def applyRestrictions(self):
         ###will determine monetary restrictions prior to ranking
-        monetaryRestrictionsList = {
-            "gaming":[.20,.40,.15,.8,.9,.8],
-            "work/general/business": [.30,.30,.15,.8,.9,.8],
-            "powerhouse": [.20,.45,.10,.7,.10,.8]
-        }
+        dictLists = ["cpuDataBase.csv", "gpuDataBase.csv", "moboDataBase.csv", "memoryDataBase.csv", "storageDataBase.csv", "psuDataBase.csv"]
+        
+        typeCounter = 0
+        
+        ###Sort through all input
+        for response in self.inputList:
+            response.strip()
+            ###ignore all default choices
+            if response != "" and response != None:
+                ###sort through all data for the type of input
+                for component in dataSheet.Datasheet(dictLists[typeCounter]):
+                    ###find component
+                    if component.name == self.inputList[typeCounter]:
+                        
+                        ###subtract price from overall budget
+                        self.budget = self.budget - component.price
+                        
+                        del dictLists[typeCounter]
+                        typeCounter = typeCounter - 1
+                        
+                
+            typeCounter = typeCounter + 1
+            
+        return dictLists
+        
+                
+                
+            
+        
+        
         
         
